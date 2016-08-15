f39ee204daa052a655b331de11c7c1582ba3340f
kikani89@yahoo.com <kikani89@yahoo.com>
2016-08-15 15:35:57 +0530

    status for login

--- index.php
+++ index.php
@@ -1,14 +1,46 @@
-<?php 
-require_once ("Libs/vendor/autoload.php");
-use Abraham\TwitterOAuth\TwitterOAuth;
-$access_token="2689143570-YNTHU4sWlBwHhyJVcekJKxlfjgRmn1Kx1Tp4hUC";
-$access_token_secret="y07VwNv7RdYvWKWAZ6qWOegknZ9uTs6F2u8qZwe4txbW6";
-$consumer_key="BVWfeJz0CjFhnQlrNJJ2U8cbX";
-
-$consumer_secret="cJPdzuPqJUJcv7KOlmB4JuA0EMO2ason0ga9e0GgAlars0KFfs";
-$connection = new TwitterOAuth($conumer_key,$consumer_secret , $access_token, $access_token_secret);
-$content = $connection->get("account/verify_credentials");
-$twitterObj = new EpiTwitter($consumer_key, $consumer_secret);
-$url = $twitterObj->getAuthorizationUrl();
+session_start();
+	
+	//Include Twitter OAuth library made by Jaisen Mathai, thanks for it!
+	include 'Libs/twitter-async/EpiCurl.php';
+	include 'Libs/twitter-async/EpiOAuth.php';
+	include 'Libs/twitter-async/EpiTwitter.php';
+	include 'Libs/twitter-async/secret.php';
+	//Following code is to connect and authenticate with twitter
+	$twitterObj = new EpiTwitter($consumer_key, $consumer_secret);
+	$oauth_token = $_GET['oauth_token'];
+	//If there is no token then display sign in link
+	if($oauth_token == '')
+	{
+		//Get auth token and build Sign In url.
+		$url = $twitterObj->getAuthorizationUrl();
 echo $url;
+	}
+	//Otherwise if user got token then the to & fro part starts
+	else
+	{
+		$twitterObj->setToken($_GET['oauth_token']);
+		$token = $twitterObj->getAccessToken();
+		$twitterObj->setToken($token->oauth_token, $token->oauth_token_secret);
+		$_SESSION['ot'] = $token->oauth_token;
+		$_SESSION['ots'] = $token->oauth_token_secret;
+		//Get the credentials of user
+		$twitterInfo= $twitterObj->get_accountVerify_credentials();
+		$twitterInfo->response;
+		$username = $twitterInfo->screen_name;
+		$profilepic = $twitterInfo->profile_image_url;
+		
+			//Include tweet page which contains tweet grabbing code
+//			include 'tweet.php';
+//			echo "Tweet Showcase:";		
+//			echo "<div id='divTweetContainer' class='slideshowTweet'>";
+//				getTweets($username, 'tweet');
+//			echo "</div>";
+			
+			//Include follower page which contains followers grabbing code
+//			include 'follower.php';
+//			getFollo($username);
+//		echo "</div>";	//Right col div ends
+	}
+//	echo "</div>";	// Container div ends
 ?>
+
