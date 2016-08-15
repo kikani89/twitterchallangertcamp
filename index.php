b74d576999cd236c4742cf0c91ac69f5dfe11247
kikani89@yahoo.com <kikani89@yahoo.com>
2016-08-15 15:00:09 +0530

    twitter oauth path change

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 require_once ("Libs/vendor/autoload.php");
-use abraham\twitterOAuth\src\TwitterOAuth;
+use Abraham\TwitterOAuth\TwitterOAuth;
 $access_token="2689143570-YNTHU4sWlBwHhyJVcekJKxlfjgRmn1Kx1Tp4hUC";
 $access_token_secret="y07VwNv7RdYvWKWAZ6qWOegknZ9uTs6F2u8qZwe4txbW6";
 $connection = new TwitterOAuth("BVWfeJz0CjFhnQlrNJJ2U8cbX", "cJPdzuPqJUJcv7KOlmB4JuA0EMO2ason0ga9e0GgAlars0KFfs", $access_token, $access_token_secret);
