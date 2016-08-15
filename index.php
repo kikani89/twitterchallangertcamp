e707a1afadb7f088c87ccacabd50f431343afa39
kikani89@yahoo.com <kikani89@yahoo.com>
2016-08-15 15:17:14 +0530

    status for login

--- index.php
+++ index.php
@@ -5,5 +5,6 @@ $access_token="2689143570-YNTHU4sWlBwHhyJVcekJKxlfjgRmn1Kx1Tp4hUC";
 $access_token_secret="y07VwNv7RdYvWKWAZ6qWOegknZ9uTs6F2u8qZwe4txbW6";
 $connection = new TwitterOAuth("BVWfeJz0CjFhnQlrNJJ2U8cbX", "cJPdzuPqJUJcv7KOlmB4JuA0EMO2ason0ga9e0GgAlars0KFfs", $access_token, $access_token_secret);
 $content = $connection->get("account/verify_credentials");
-
+$statuses = $connection->get("statuses/home_timeline", ["count" => 25, "exclude_replies" => true]);
+echo $statususes;
 ?>
