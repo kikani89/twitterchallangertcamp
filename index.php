0a4416195699ec4bc9e0f6e88fd93174af77f58a
kikani89@yahoo.com <kikani89@yahoo.com>
2016-08-15 18:32:24 +0530

    Api change

--- index.php
+++ index.php
@@ -1,49 +1,51 @@
-<?php
-session_start();
-	
-	//Include Twitter OAuth library made by Jaisen Mathai, thanks for it!
-	include 'Libs/EpiCurl.php';
-	include 'Libs/EpiOAuth.php';
-	include 'Libs/EpiTwitter.php';
-	include 'Libs/secret.php';
-	//Following code is to connect and authenticate with twitter
-	$twitterObj = new EpiTwitter($consumer_key, $consumer_secret);
-	$oauth_token = $_GET['oauth_token'];
-	//If there is no token then display sign in link
-	if($oauth_token == '')
-	{
-		//Get auth token and build Sign In url.
-		$url = $twitterObj->getAuthorizationUrl();
-        echo "helloinside";
-echo "<a href='$url'>Please Sign In through Your Twitter Account</a>";
-	}
-	//Otherwise if user got token then the to & fro part starts
-	else
-	{
-        echo "hello";
-		$twitterObj->setToken($_GET['oauth_token']);
-		$token = $twitterObj->getAccessToken();
-		$twitterObj->setToken($token->oauth_token, $token->oauth_token_secret);
-		$_SESSION['ot'] = $token->oauth_token;
-		$_SESSION['ots'] = $token->oauth_token_secret;
-		//Get the credentials of user
-		$twitterInfo= $twitterObj->get_accountVerify_credentials();
-		$twitterInfo->response;
-		$username = $twitterInfo->screen_name;
-		$profilepic = $twitterInfo->profile_image_url;
-		
-			//Include tweet page which contains tweet grabbing code
-//			include 'tweet.php';
-//			echo "Tweet Showcase:";		
-//			echo "<div id='divTweetContainer' class='slideshowTweet'>";
-//				getTweets($username, 'tweet');
-//			echo "</div>";
-			
-			//Include follower page which contains followers grabbing code
-//			include 'follower.php';
-//			getFollo($username);
-//		echo "</div>";	//Right col div ends
-	}
-//	echo "</div>";	// Container div ends
-?>
 
++<?php
+require_once "Libs/codebird.php";
+include "Libs/secret.php";
+use Codebird/Codebird;
+
+Codebird::setConsumerKey($consumer_key,$consumer_secret);
+$cd=Codebird::getInstance();
+$cd->setToken($access_token,$access_token_secret);
+
+session_start();
+
+if (! isset($_SESSION['oauth_token'])) {
+  // get the request token
+  $reply = $cb->oauth_requestToken([
+    'oauth_callback' => 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']
+  ]);
+
+  // store the token
+  $cb->setToken($reply->oauth_token, $reply->oauth_token_secret);
+  $_SESSION['oauth_token'] = $reply->oauth_token;
+  $_SESSION['oauth_token_secret'] = $reply->oauth_token_secret;
+  $_SESSION['oauth_verify'] = true;
+
+  // redirect to auth website
+  $auth_url = $cb->oauth_authorize();
+  header('Location: ' . $auth_url);
+  die();
+
+} elseif (isset($_GET['oauth_verifier']) && isset($_SESSION['oauth_verify'])) {
+  // verify the token
+  $cb->setToken($_SESSION['oauth_token'], $_SESSION['oauth_token_secret']);
+  unset($_SESSION['oauth_verify']);
+
+  // get the access token
+  $reply = $cb->oauth_accessToken([
+    'oauth_verifier' => $_GET['oauth_verifier']
+  ]);
+
+  // store the token (which is different from the request token!)
+  $_SESSION['oauth_token'] = $reply->oauth_token;
+  $_SESSION['oauth_token_secret'] = $reply->oauth_token_secret;
+
+  // send to same URL, without oauth GET parameters
+  header('Location: ' . basename(__FILE__));
+  die();
+}
+
+// assign access token on each page load
+$cb->setToken($_SESSION['oauth_token'], $_SESSION['oauth_token_secret']);
+?>
